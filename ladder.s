db6ec9e03eaeac78b371b3f475db02c1dc8578ac
JLopano <gzistns@gmail.com>
2017-12-10 22:08:42 -0800

    Assembly Ladder Game

---
+++ ladder.s
@@ -0,0 +1,21 @@
+.equ LOW,    0
+.equ HIGH,   0
+.equ INPUT,  0
+.equ OUTPUT, 1
+
+//LED WiringPi Configuration
+.equ BLUETWO, 21
+.equ BLUEONE, 22
+.equ YELLOW,  23
+.equ RED,     24
+
+//Button WiringPu Configuration
+.equ BOTTON,  0
+
+.data
+.text
+.global main
+
+main:
+	push {lr}
+
